e426e9c07ad862ab7261a0d70efdf2639a300fa2
Yaroslav9728 <31794068+Yaroslav9728@users.noreply.github.com>
2022-02-22 15:43:04 -0500

    Added configuration for database connection, and SportContext DB model. Please finish OnModelCreating method by filling data for each entity.

--- SportsPro/SportsPro/Program.cs
+++ SportsPro/SportsPro/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using SportsPro.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,6 +15,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+builder.Services.AddDbContext<SportContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SportContext"));
+}
+);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
