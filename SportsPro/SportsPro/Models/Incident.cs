8118d86a76745657ecbdfb04f383245ad8817390
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2022-02-22 18:46:10 -0900

    added data validation for incident

--- SportsPro/SportsPro/Models/Incident.cs
+++ SportsPro/SportsPro/Models/Incident.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
 
 namespace SportsPro.Models
 {
@@ -11,22 +12,24 @@ namespace SportsPro.Models
         public string? Title { get; set; }   
 
         [Required]
+
         public int CustomerId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please choose customer from dropdown")]
         public Customer? Customer { get; set; }
 
-        [Required]
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = "Please choose product from dropdown")]
         public Product? Product { get; set; }
 
         public DateTime DateOpened { get; set; }
+        [Required(ErrorMessage = "Please choose date when incident was closed")]
         public DateTime DateClosed { get; set; }
 
         public int TechnicianId { get; set; }
         public Technician? Technician { get; set; }
-
+        [Required(ErrorMessage = "Please add incident description")]
         public string? Description { get; set; }
      }
 }
