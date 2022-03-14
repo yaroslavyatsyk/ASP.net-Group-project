27006127bd728081d80f2a1b31742f0aa0349165
Yaroslav9728 <31794068+Yaroslav9728@users.noreply.github.com>
2022-03-14 03:00:44 -0400

    Started adding data validation for customer

--- SportsPro/SportsPro/Models/Customer.cs
+++ SportsPro/SportsPro/Models/Customer.cs
@@ -20,15 +20,18 @@ namespace SportsPro.Models
         public string? State { get; set; }
 
         [Required(ErrorMessage = "Please enter postal code.")]
+        [DataType(DataType.PostalCode)]
         public string? PostalCode { get; set; }
 
         [Required(ErrorMessage = "Please enter country.")]
         public string? Country{ get; set; }
 
         [Required(ErrorMessage = "Please enter email.")]
+        [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Please enter the phone number.")]
+        [DataType(DataType.PhoneNumber,ErrorMessage = "The phone number must be in format (XXX)-XXX-XXXX")]
         public string? Phone { get; set; }
 
         public string FullName => FirstName?.Replace(' ', '-') + " " + LastName?.Replace(' ', '-');
