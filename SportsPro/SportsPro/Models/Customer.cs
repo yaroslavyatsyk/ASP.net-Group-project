26eb85758938233842b906e8d15b8f5e1e11613f
Yaroslav9728 <31794068+Yaroslav9728@users.noreply.github.com>
2022-03-14 03:41:11 -0400

    Updated data validation

--- SportsPro/SportsPro/Models/Customer.cs
+++ SportsPro/SportsPro/Models/Customer.cs
@@ -8,19 +8,24 @@ namespace SportsPro.Models
         public int CustomerId { get; set; }
 
         [Required(ErrorMessage = "Please enter first name.")]
+        [StringLength(50, MinimumLength = 1,ErrorMessage = "The First Name must have at least 1 character and not more than or equal 50")]
         public string? FirstName { get; set; }
 
         [Required(ErrorMessage = "Please enter last name.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "The First Name must have at least 1 character and not more than or equal 50")]
         public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Please enter city.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "The First Name must have at least 1 character and not more than or equal 50")]
         public string? City { get; set; }
 
         [Required(ErrorMessage = "Please enter state.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "The First Name must have at least 1 character and not more than or equal 50")]
         public string? State { get; set; }
 
         [Required(ErrorMessage = "Please enter postal code.")]
         [DataType(DataType.PostalCode)]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "The First Name must have at least 1 character and not more than or equal 20")]
         public string? PostalCode { get; set; }
 
         [Required(ErrorMessage = "Please enter country.")]
@@ -28,10 +33,12 @@ namespace SportsPro.Models
 
         [Required(ErrorMessage = "Please enter email.")]
         [DataType(DataType.EmailAddress)]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "The First Name must have at least 1 character and not more than or equal 50")]
+        
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "Please enter the phone number.")]
-        [DataType(DataType.PhoneNumber,ErrorMessage = "The phone number must be in format (XXX)-XXX-XXXX")]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
         public string? Phone { get; set; }
 
         public string FullName => FirstName?.Replace(' ', '-') + " " + LastName?.Replace(' ', '-');
