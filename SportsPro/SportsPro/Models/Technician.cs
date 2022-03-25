f454bffb906ba4e3bce2757cde62977147a353d0
Yaroslav9728 <31794068+Yaroslav9728@users.noreply.github.com>
2022-03-24 22:13:43 -0400

    Updated Technician

--- SportsPro/SportsPro/Models/Technician.cs
+++ SportsPro/SportsPro/Models/Technician.cs
@@ -14,9 +14,12 @@ namespace SportsPro.Models
         public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Please enter email.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Incorrect format")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "The First Name must have at least 1 character and not more than or equal 50")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "Please enter phone.")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
         public string? Phone { get; set; }
 
         public string FullName => FirstName?.Replace(' ', '-') + " " + LastName?.Replace(' ', '-');
